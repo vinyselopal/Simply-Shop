67637e0bc320425db44a3190ae79932c53b6ace0
viny <viny0698@gmail.com>
2022-12-30 15:09:20 +0530

    add markup for checkout page

--- frontend/src/Checkout/index.js
+++ frontend/src/Checkout/index.js
@@ -8,62 +8,9 @@ import { useSelectorWrapper } from '../utils'
 function Checkout () {
   const dispatch = useDispatch()
   const navigate = useNavigate()
-
-  const cart = useSelectorWrapper('cart')
-  const token = useSelectorWrapper('token')
-  const order = useSelectorWrapper('order')
-
-  const [orderID, setOrderID] = useState(JSON.parse(localStorage.getItem('order'))?.orderID)
-
-  useEffect(() => {
-    if (!order) {
-      const productsIdArray = []
-
-      cart.forEach(item => {
-        productsIdArray.push(item.item.id)
-      })
-      const order = {
-        productsIdArray
-      }
-      dispatch(setOrder(order))
-      createOrder(token, productsIdArray).then(orderID => {
-        setOrderID(orderID)
-        dispatch(setOrder({ ...order, orderID }))
-      })
-    }
-
-    if (order) {
-      const productsIdArray = []
-
-      cart.forEach(item => {
-        productsIdArray.push(item.item.id)
-      })
-      const order = {
-        productsIdArray
-      }
-      dispatch(setOrder(order))
-      cancellationHandler().then(() => {
-        createOrder(token, productsIdArray).then(orderID => {
-          setOrderID(orderID)
-          dispatch(setOrder({ ...order, orderID }))
-        })
-      })
-    }
-  }, [])
-
-  function renderOptions () {
-    navigate('/options')
-  }
-  const cancellationHandler = async () => {
-    await cancelOrder(token, orderID)
-    dispatch(setOrder(null))
-    localStorage.removeItem('order')
-  }
-
-  const cancellationListener = async () => {
-    await cancellationHandler()
-    navigate('/cart')
-  }
+  // const total = useSelectorWrapper('cart')
+  const total = 500
+  const addresses = ['add1', 'add2']
   return (
     <div>
       <Link to='/' className='no-underline text-black pl-5 position-absolute'><h2>Simply Shop</h2></Link>
@@ -71,10 +18,30 @@ function Checkout () {
       <div className='flex bg-gray-300 justify-center p-2'>
         <h2 className='pl-5'>Checkout</h2>
       </div>
-      <div className='flex justify-center m-4'>
-        <button className='bg-amber-500 p-2 m-2' onClick={renderOptions}>Continue</button>
-        <button className='bg-amber-500 p-2 m-2' onClick={cancellationListener}>Cancel order</button>
+      <div className='flex flex-row justify-center m-4'>
+        <div className='flex flex-col m-4 basis-3/4'>
+          <div className='checkout_address_container'>
+            <h4>1. Select a delivery address</h4>
+            <div className='border-2 border-solid flex flex-col p-4'>
+
+              {
+                addresses.map((address, index) => (
+                 
+                ))
+              }
+            </div>
+          </div>
+          <div className='checkout_payment_container'>
+            <h4>2. Payment Method</h4>
+          </div>
+        </div>
+        <div className='order_summary basis-1/4 border-2 border-solid p-4 m-4'>
+          <h4>Order Summary</h4>
+          <div>Total Amount: Rs. {total}</div>
+          <button className='bg-amber-500 p-2 mt-4 rounded-2'>Place your order and pay</button>
+        </div>
       </div>
+
     </div>
   )
 }
