1349198fd9899d054fb778cb4cbe66d01f27c35b
viny <viny0698@gmail.com>
2023-01-06 11:34:25 +0530

    create basic layout for orders page using flex

--- frontend/src/Orders/index.js
+++ frontend/src/Orders/index.js
@@ -13,17 +13,24 @@ function Orders () {
   }, [])
 
   return (
-    <div className=' mt-16 mr-4 ml-4 p-4'>
-      <h3>Orders</h3>
-      <ul>
-        {
+    <div className='m-auto flex justify-center'>
+      <div className=' mt-16 mr-4 ml-4 p-4 flex flex-col w-3/5 items-stretch'>
+        <div className='flex flex-col'>
+          <h3 className='border-b-2'>My Orders</h3>
+        </div>
+        <div>Total number of orders</div>
+        <ul>
+          {
           orders?.map((order, index) => (
             <li key={index}>
-              order ID : {order.id}
+              <div className='m-4 p-2 rounded  border-2 border-slate-200'>
+                order ID : {order.id}
+              </div>
             </li>
           ))
         }
-      </ul>
+        </ul>
+      </div>
     </div>
   )
 }
